f0f24db2db482c6421fe596c03704a102731518d
sdmengxiangyu <sdmengxiangyu@gmail.com>
2016-12-25 22:12:27 +0800

    页面跳转

--- StartActivity1/app/src/main/java/mxy/com/startactivity1/Activity1.java
+++ StartActivity1/app/src/main/java/mxy/com/startactivity1/Activity1.java
@@ -1,10 +1,47 @@
 package mxy.com.startactivity1;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by asus on 2016/12/25.
  */
 
 public class Activity1 extends AppCompatActivity {
+
+    private TextView txt;
+    private Button btnButton;
+    public final static int RESULT_CODE = 1;
+
+    private View.OnClickListener listener=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent=new Intent();
+            //intent.setClass(Activity1.this,Activity1.class);
+            intent.putExtra("back","第二个activity传过来的值");
+            //startActivity(intent);
+            //startActivityForResult(intent,REQUEST_CODE);
+            setResult(RESULT_CODE,intent);
+            finish();
+        }
+    };
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.link);
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        String str = bundle.getString("str");
+        txt = (TextView)findViewById(R.id.Activity1_text);
+        txt.setText(str);
+        btnButton = (Button)findViewById(R.id.Activity1_btn);
+        btnButton.setOnClickListener(listener);
+    }
 }
--- StartActivity1/app/src/main/java/mxy/com/startactivity1/MainActivity.java
+++ StartActivity1/app/src/main/java/mxy/com/startactivity1/MainActivity.java
@@ -1,21 +1,38 @@
 package mxy.com.startactivity1;
 
+import android.app.Activity;
+import android.app.DownloadManager;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static int REQUEST_CODE = 1;
+
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode, Intent data){
+        if(requestCode == REQUEST_CODE){
+            if(resultCode == Activity1.RESULT_CODE){
+                Bundle bundle= data.getExtras();
+            String str = bundle.getString("back");
+                Toast.makeText(MainActivity.this,str,Toast.LENGTH_LONG).show();}
+        }
+    }
 
     private View.OnClickListener listener=new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             Intent intent=new Intent();
             intent.setClass(MainActivity.this,Activity1.class);
-            startActivity(intent);
+            intent.putExtra("str","第一个activity传过来的值");
+            //startActivity(intent);
+            startActivityForResult(intent,REQUEST_CODE);
         }
-    }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
